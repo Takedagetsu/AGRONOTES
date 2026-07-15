c1e431df209a6cc8d84ab21c0f6298c7ce96db81
Rafael Neves <rfl_simoes@outlook.com>
2026-07-14 22:56:55 -0300

    Update script.js

--- scr/script/script.js
+++ scr/script/script.js
@@ -1,26 +1,24 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAhsOmcupXfBZoYclOl1gdGLW0PI-JBfp4",
-    authDomain: "agronotes-8b1bf.firebaseapp.com",
-    projectId: "agronotes-8b1bf",
-    storageBucket: "agronotes-8b1bf.firebasestorage.app",
-    messagingSenderId: "678118849161",
-    appId: "1:678118849161:web:caea28422fd43e3bb3a458",
-    measurementId: "G-5NFRZYHXMV"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhsOmcupXfBZoYclOl1gdGLW0PI-JBfp4",
+  authDomain: "agronotes-8b1bf.firebaseapp.com",
+  projectId: "agronotes-8b1bf",
+  storageBucket: "agronotes-8b1bf.firebasestorage.app",
+  messagingSenderId: "678118849161",
+  appId: "1:678118849161:web:caea28422fd43e3bb3a458",
+  measurementId: "G-5NFRZYHXMV"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 // Seleção dos elementos do HTML
